bf1b27436d1564a4ba1e311e22f8b8535fab90c9
Vivecboots <Blairwin05@gmail.com>
2023-07-30 01:22:30 -0700

    lets try new container for donation

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ export function App() {
 
   return (
     <>
-      <h1>wagmi + RainbowKit + Vite</h1>
+      <h1 fontFamily="monospace" >Auth-0/Crypto-auth Boilerplate</h1>
 
       <LoginButton />
       <LogoutButton />
