ca437a4bcc915b2ee19c5733f09e89fc74a78427
Vivecboots <Blairwin05@gmail.com>
2023-07-30 18:33:01 -0700

    changed main.tsx to js file

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { WagmiConfig } from 'wagmi';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+import { App } from './App';
+import { config } from './wagmi';
+
+const auth0ProviderOptions = {
+  domain: "dev-izis0xfcx8kwkhd3.us.auth0.com",
+  clientId: "1kB9ySvN42L1N0pUNtP4OHnbGskosQ5D",
+  redirectUri: window.location.origin,
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  React.createElement(
+    React.StrictMode,
+    {},
+    React.createElement(
+      WagmiConfig,
+      { config: config },
+      React.createElement(
+        Auth0Provider,
+        auth0ProviderOptions,
+        React.createElement(App)
+      )
+    )
+  )
+);
